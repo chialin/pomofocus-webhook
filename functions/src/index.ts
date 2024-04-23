5a09b27a7d4de2583c7a4d49c87ade818eaacd8a
chialin <chialin.shr@gmail.com>
2024-04-23 13:37:03 +0800

    Add pomofocus webhook data to firebase DB

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,44 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+import { initializeApp, database } from 'firebase-admin';
 import { onRequest } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
-
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+interface PomoData {
+  type: string;
+  round: string;
+}
+
+initializeApp({
+  databaseURL: process.env.DATABASE_URL,
+});
+
+const pomoWebhook = onRequest((request, response) => {
+  // get data from Pomofocus webhook
+  const pomoData: PomoData = request.body;
+  logger.info('pomoData', { data: pomoData });
+  // get type
+  const eventType: string = pomoData.type;
+  const round: string = pomoData.round;
+
+  const pomoStatusToDB: PomoData = {
+    type: eventType,
+    round,
+  };
+
+  logger.info('pomoFinalStatus', pomoStatusToDB);
+  database()
+    .ref('/pomoData/')
+    .set(pomoStatusToDB)
+    .then(() => {
+      response.status(200).send('Data saved successfully!');
+    })
+    .catch((error) => {
+      response.status(500).send(error);
+    });
+});
+
+module.exports = pomoWebhook;
